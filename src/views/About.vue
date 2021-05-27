468d91360aa20002c70fdf74ca012240892be93f
Calmes <77333391+ChristopherKerhman@users.noreply.github.com>
2021-05-27 21:54:34 +0200

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>L'application de la poulailler</h1>
+    <p>
+      Idée originale : Camille
+      Réalisation : Christophe
+    </p>
   </div>
 </template>
